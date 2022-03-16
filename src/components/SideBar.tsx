454979ef47fcdfe7d6ffba4fd4273898918f4400
Weydson Marinho <weydsonmarinho@gmail.com>
2022-03-16 10:10:24 -0300

    Complete the challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,40 @@
-export function SideBar() {
+import { Button } from "./Button";
+
+interface GenreProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface SidebarProps {
+  genres: GenreProps[];
+  onClickButton: (genreId: number) => void;
+  featuredGenreId: number;
+}
+
+export function SideBar({
+  genres,
+  onClickButton,
+  featuredGenreId,
+}: SidebarProps) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre: GenreProps) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClickButton(genre.id)}
+            selected={featuredGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
